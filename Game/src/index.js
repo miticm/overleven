5aeea112ff04420b04fb75f4da4f639834b5a370
benkahlert <bkahlert@purdue.edu>
2019-02-03 03:11:21 -0500

    Basic player movement

--- Game/src/index.js
+++ Game/src/index.js
@@ -7,28 +7,54 @@ var config = {
     height: 600,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update,
+    },
+    physics: {
+        default: 'arcade'
     }
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
+
+let player;
+let controls;
 
 function preload ()
 {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('grass', 'assets/grass.png')
+    this.load.spritesheet('player', 'assets/player_sheet.png', {
+        frameWidth: 32,
+        frameHeight: 32
+    });
 }
 
 function create ()
 {
-    var logo = this.add.image(400, 150, 'logo');
+    const grass = this.add.image(400, 300, 'grass');
+    player = this.physics.add.sprite(20, 20, 'player');
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
+    controls = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (controls.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (controls.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (controls.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (controls.down.isDown) {
+        player.setVelocityY(160);
+    } else {
+        player.setVelocityY(0);
+    }
 }
