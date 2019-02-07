aaf8c37982e66022e6b450d682611dd5f71d34e7
Tianchi Xu <xtc459924276@gmail.com>
2019-02-07 16:41:58 -0500

    drop and pick up items

--- Game/src/generic-functions.js
+++ Game/src/generic-functions.js
@@ -3,8 +3,12 @@ import { BLOCK_SIZE } from "./constants";
 
 // Returns an object describing which block the coordinates are in
 export function currentBlock(x, y) {
-    return {
-        x: Math.floor(x / BLOCK_SIZE),
-        y: Math.floor(y / BLOCK_SIZE)
-    };
+  return {
+    x: Math.floor(x / BLOCK_SIZE),
+    y: Math.floor(y / BLOCK_SIZE)
+  };
+}
+
+export function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
