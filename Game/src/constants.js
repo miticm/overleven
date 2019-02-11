a171052c615f33c0ad666e654bf5f63456aed91f
benkahlert <bkahlert@purdue.edu>
2019-02-11 13:04:19 -0500

    Click to move player

--- Game/src/constants.js
+++ Game/src/constants.js
@@ -1,6 +1,6 @@
 // Constants for the whole file
-export const HEIGHT = 600;
-export const WIDTH = 800;
+export const HEIGHT = 640;
+export const WIDTH = 896;
 export const BLOCK_SIZE = 64;
 
 // Computed
