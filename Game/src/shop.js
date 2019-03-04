22f0a096c45b86617e565c239db7bcf2966928f8
Trevor Neidlinger <trevorneidlinger@Trevors-MacBook-Pro.local>
2019-03-04 18:47:43 -0500

    added dmg buff in shop and changed gold when killing from 200 to 25

--- Game/src/shop.js
+++ Game/src/shop.js
@@ -7,6 +7,8 @@ import {
 import { hp, gold, maxHealth, maxPlayerSpeed} from "./game";
 let shieldBought = false;
 let speedBought = false;
+let dmgBought = 0;
+let goldForDamage = 50;
 
 
 export const shop = new Phaser.Class({
@@ -22,6 +24,7 @@ export const shop = new Phaser.Class({
     this.load.image("resume_button", "assets/resume_button.png");
     this.load.image("shield", "assets/shield.png");
     this.load.image("speed", "assets/speed.png");
+    this.load.image("dmg", "assets/dmg.png");
   },
 
   create: function () {
@@ -33,6 +36,10 @@ export const shop = new Phaser.Class({
     const text1 = this.add.text(WIDTH / 3, HEIGHT / 5, "Inc Max Health ($200)", {
         fontSize: "32px"
     });
+
+    if(shieldBought == true){
+        text1.setText("SOLD");
+    }
 
     //shield image
     let shield = this.add
@@ -46,7 +53,7 @@ export const shop = new Phaser.Class({
 
     shield.on("pointerover", () => {
       //make play button bloom
-        shield.setScale(0.25, 0.25);
+        shield.setScale(0.18, 0.18);
     });
     shield.on("pointerout", () => {
       //reset button bloom
@@ -60,24 +67,25 @@ export const shop = new Phaser.Class({
                 if(shieldBought == false){
                     this.events.emit("goldByShield");
                     shieldBought = true;
+                    text1.setText("SOLD");
                 }
             }
         },
         this
     );
-    //hide shield so it is known it is bought
-    if(shieldBought == true){
-        shield.visible = false;
-    }
     
-    const text2 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 50, "Inc Max Speed ($100)", {
+    const text2 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 60, "Inc Max Speed ($100)", {
     fontSize: "32px"
     });
+    
+    if(speedBought == true){
+        text2.setText("SOLD");
+    }
 
     //speed image
     let speed = this.add
       .sprite(
-        (WIDTH / 3) - 50, (HEIGHT / 5) + 50,
+        (WIDTH / 3) - 50, (HEIGHT / 5) + 60,
         "speed"
       )
       .setDepth(1);
@@ -86,7 +94,7 @@ export const shop = new Phaser.Class({
 
     speed.on("pointerover", () => {
       //make play button bloom
-        speed.setScale(0.25, 0.25);
+        speed.setScale(0.18, 0.18);
     });
     speed.on("pointerout", () => {
       //reset button bloom
@@ -101,22 +109,63 @@ export const shop = new Phaser.Class({
                 if(speedBought == false){
                     this.events.emit("goldBySpeed");
                     speedBought = true;
+                    text2.setText("SOLD");
                 }
                 
             }
         },
         this
     );
-    //hide speed so it is known it is bought
-    if(speedBought == true){
-        speed.visible = false;
-    }
 
-    const text3 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 100, "Buy", {
+    const text3 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 120, `Inc Q dmg ($${goldForDamage})`, {
         fontSize: "32px"
     });
 
-    const text4 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 150, "Buy", {
+    if(dmgBought == 3){
+        text3.setText("SOLD");
+    }
+
+    //speed image
+    let dmg = this.add
+      .sprite(
+        (WIDTH / 3) - 50, (HEIGHT / 5) + 120,
+        "dmg"
+      )
+      .setDepth(1);
+    dmg.setScale(0.15, 0.15);
+    dmg.setInteractive();
+
+    dmg.on("pointerover", () => {
+      //make play button bloom
+        dmg.setScale(0.18, 0.18);
+    });
+    dmg.on("pointerout", () => {
+      //reset button bloom
+        dmg.setScale(0.15, 0.15);
+    });
+
+    dmg.on(
+        "pointerup",
+        function (event) {
+            if(gold >= goldForDamage){
+                console.log(gold);
+                if(dmgBought != 3){
+                    this.events.emit("goldByDmg");
+                    dmgBought++;
+                    goldForDamage *= 3;
+                    if(dmgBought != 3){
+                        text3.setText(`Inc Q dmg ($${goldForDamage})`);
+                    } else {
+                        text3.setText("SOLD");
+                    }
+                }
+                
+            }
+        },
+        this
+    );
+
+    const text4 = this.add.text(WIDTH / 3, (HEIGHT / 5) + 180, "Buy", {
         fontSize: "32px"
       });
 
