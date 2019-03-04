5c489cfdfa8a4d3ad70e0b863ed2b4d8cea6533b
Daniel Hipskind <dhipskind253@Daniels-MacBook-Pro-2.local>
2019-03-04 16:39:04 -0500

    Cannot teleport into terrain anymore

--- Game/src/game.js
+++ Game/src/game.js
@@ -426,13 +426,13 @@ function playerMove() {
   //distance tolerance
   var distance = Phaser.Math.Distance.Between(player.x, player.y, target.x, target.y);
 
-  if (player.body.speed > 0) {
+  if (Math.abs(player.body.velocity.x) > 5 || Math.abs(player.body.velocity.y) > 5) {
     //check for distance tolerance
     if (distance < 4) {
       player.setVelocity(0);
     }
     else if ((player.body.velocity.x == 0 || player.body.velocity.y == 0) && target.x != player.body.velocity.x && target.y != player.body.velocity.y) {
-      Scene.physics.moveToObject(player, target, playerSpeed);
+        Scene.physics.moveToObject(player, target, playerSpeed);
     }
     
     //animations
@@ -450,6 +450,7 @@ function playerMove() {
     }
   }
   else {
+    player.setVelocity(0);
     player.anims.play("idle", true);
   }
 }
@@ -697,5 +698,6 @@ function addEnemy(x, y) {
 }
 
 function stopPlayer(player, grounds) {
-  moving = false;
+  player.x = this.game.renderer.width / 2;
+  player.y = this.game.renderer.height / 2;
 }
