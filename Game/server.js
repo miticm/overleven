e46fbd767db79de19137255141d61901a5a7fd24
Tianchi Xu <xtc459924276@gmail.com>
2019-04-10 19:32:14 -0400

    server

---
+++ Game/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/'));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
